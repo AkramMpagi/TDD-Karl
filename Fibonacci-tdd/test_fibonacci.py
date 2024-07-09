878098ce9eab5cdeb1842d5088ff41f6f8abbea2
Akram Mpagi <akrammpagi06@gmail.com>
2024-07-09 18:17:18 +0300

    Add Jovita, Hussein, Akram

--- Fibonacci-tdd/test_fibonacci.py
+++ Fibonacci-tdd/test_fibonacci.py
@@ -8,3 +8,8 @@ def test_fibonacci_1():
 def test_fibonacci_2():
     assert fibonacci(2) == [0, 1]
 
+def test_fibonacci_3():
+    assert fibonacci(3) == [0, 1, 1]
+
+def test_fibonacci_20():
+    assert fibonacci(20) == [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181]
